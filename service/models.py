c8beb75b262b6a4faf370534bdef92186974f3f5
red <red@redrybin.me>
2024-03-02 23:29:47 +0000

    Added Models and Factories and tests for Models - Added Orders and OrderItems models - Added Factories for Orders and OrderItems - Added Tests for Orders and OrderItems

--- service/models.py
+++ service/models.py
@@ -5,7 +5,9 @@ All of the models are stored in this module
 """
 
 import logging
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Enum
 
 logger = logging.getLogger("flask.app")
 
@@ -17,28 +19,46 @@ class DataValidationError(Exception):
     """Used for an data validation errors when deserializing"""
 
 
-class YourResourceModel(db.Model):
+class Orders(db.Model):
     """
-    Class that represents a YourResourceModel
+    Class that represents a order
     """
 
     ##################################################
     # Table Schema
     ##################################################
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
-
-    # Todo: Place the rest of your schema here...
+    order_id: int = db.Column(db.Integer, primary_key=True)
+    customer_id: int = db.Column(db.Integer)
+    order_date: datetime = db.Column(db.DateTime)
+    status: str = db.Column(
+        Enum(
+            "pending",
+            "processing",
+            "shipped",
+            "delivered",
+            "cancelled",
+            "returned",
+            "refunded",
+            name="status_enum",
+        ),
+        default="pending",
+    )
+    tracking_number: str = db.Column(db.String)
+    discount_amount: float = db.Column(db.Float)
+    # Relationship to OrderItems
+    order_items = db.relationship(
+        "OrderItems", backref="orders", cascade="all, delete-orphan"
+    )
 
     def __repr__(self):
-        return f"<YourResourceModel {self.name} id=[{self.id}]>"
+        return f"<Order id=[{self.order_id}]>"
 
     def create(self):
         """
-        Creates a YourResourceModel to the database
+        Creates a Order to the database
         """
-        logger.info("Creating %s", self.name)
-        self.id = None  # pylint: disable=invalid-name
+        logger.info("Creating %s", self.order_id)
+        self.order_id = None  # pylint: disable=invalid-name
         try:
             db.session.add(self)
             db.session.commit()
@@ -49,9 +69,9 @@ class YourResourceModel(db.Model):
 
     def update(self):
         """
-        Updates a YourResourceModel to the database
+        Updates a Order to the database
         """
-        logger.info("Saving %s", self.name)
+        logger.info("Saving %s", self.order_id)
         try:
             db.session.commit()
         except Exception as e:
@@ -60,8 +80,8 @@ class YourResourceModel(db.Model):
             raise DataValidationError(e) from e
 
     def delete(self):
-        """Removes a YourResourceModel from the data store"""
-        logger.info("Deleting %s", self.name)
+        """Removes a Order from the data store"""
+        logger.info("Deleting %s", self.order_id)
         try:
             db.session.delete(self)
             db.session.commit()
@@ -71,18 +91,29 @@ class YourResourceModel(db.Model):
             raise DataValidationError(e) from e
 
     def serialize(self):
-        """Serializes a YourResourceModel into a dictionary"""
-        return {"id": self.id, "name": self.name}
+        """Serializes a Order into a dictionary"""
+        return {
+            "order_id": self.order_id,
+            "customer_id": self.customer_id,
+            "order_date": self.order_date.isoformat(),
+            "status": self.status,
+            "tracking_number": self.tracking_number,
+            "discount_amount": self.discount_amount,
+        }
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
+        Deserializes a Order from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
         """
         try:
-            self.name = data["name"]
+            self.customer_id: int = data["customer_id"]
+            self.order_date: datetime = datetime.fromisoformat(data["order_date"])
+            self.status: str = data["status"]
+            self.tracking_number: str = data["tracking_number"]
+            self.discount_amount: float = data["discount_amount"]
         except AttributeError as error:
             raise DataValidationError("Invalid attribute: " + error.args[0]) from error
         except KeyError as error:
@@ -101,23 +132,181 @@ class YourResourceModel(db.Model):
     ##################################################
 
     @classmethod
-    def all(cls):
-        """Returns all of the YourResourceModels in the database"""
-        logger.info("Processing all YourResourceModels")
+    def list_all(cls):
+        """Returns a list of all orders"""
+        logger.info("Processing list_all request")
         return cls.query.all()
 
     @classmethod
-    def find(cls, by_id):
-        """Finds a YourResourceModel by it's ID"""
-        logger.info("Processing lookup for id %s ...", by_id)
-        return cls.query.session.get(cls, by_id)
+    def create_new(cls, data):
+        """Creates a new order"""
+        logger.info("Processing create_new request")
+        order = cls()
+        order.deserialize(data)
+        order.create()
+        return order
 
     @classmethod
-    def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
+    def find(cls, order_id):
+        """Returns a single order by its ID"""
+        logger.info("Processing get_order request for id %s", order_id)
+        return cls.query.get(order_id)
+
+    @classmethod
+    def update_order(cls, order_id, data):
+        """Updates an order by its ID"""
+        logger.info("Processing update_order request for id %s", order_id)
+        order = cls.query.get(order_id)
+        if not order:
+            return None
+        for key, value in data.items():
+            setattr(order, key, value)
+        order.update()
+        return order
+
+    @classmethod
+    def delete_order(cls, order_id):
+        """Deletes an order by its ID"""
+        logger.info("Processing delete_order request for id %s", order_id)
+        order = cls.query.get(order_id)
+        if not order:
+            return None
+        order.delete()
+        return order
+
+
+class OrderItems(db.Model):
+    """
+    Class that represents an order item
+    """
+
+    ##################################################
+    # Table Schema
+    ##################################################
+    order_item_id: int = db.Column(db.Integer, primary_key=True)
+    order_id: int = db.Column(db.Integer, db.ForeignKey("orders.order_id"))
+    product_id: int = db.Column(db.Integer)
+    quantity: int = db.Column(db.Integer)
+    price: float = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"<OrderItem id=[{self.order_item_id}]>"
+
+    def create(self):
+        """
+        Creates an OrderItem to the database
+        """
+        logger.info("Creating %s", self.order_item_id)
+        self.order_item_id = None  # pylint: disable=invalid-name
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            logger.error("Error creating record: %s", self)
+            raise DataValidationError(e) from e
+
+    def update(self):
+        """
+        Updates an OrderItem to the database
+        """
+        logger.info("Saving %s", self.order_item_id)
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            logger.error("Error updating record: %s", self)
+            raise DataValidationError(e) from e
+
+    def delete(self):
+        """Removes an OrderItem from the data store"""
+        logger.info("Deleting %s", self.order_item_id)
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            logger.error("Error deleting record: %s", self)
+            raise DataValidationError(e) from e
+
+    def serialize(self):
+        """Serializes an OrderItem into a dictionary"""
+        return {
+            "order_item_id": self.order_item_id,
+            "order_id": self.order_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "price": self.price,
+        }
+
+    def deserialize(self, data):
+        """
+        Deserializes an OrderItem from a dictionary
 
         Args:
-            name (string): the name of the YourResourceModels you want to match
+            data (dict): A dictionary containing the resource data
         """
-        logger.info("Processing name query for %s ...", name)
-        return cls.query.filter(cls.name == name)
+        try:
+            self.product_id: int = data["product_id"]
+            self.quantity: int = data["quantity"]
+            self.price: float = data["price"]
+        except AttributeError as error:
+            raise DataValidationError("Invalid attribute: " + error.args[0]) from error
+        except KeyError as error:
+            raise DataValidationError(
+                "Invalid OrderItem: missing " + error.args[0]
+            ) from error
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid OrderItem: body of request contained bad or no data "
+                + str(error)
+            ) from error
+        return self
+
+    ##################################################
+    # CLASS METHODS
+    ##################################################
+
+    @classmethod
+    def find_by_order(cls, order_id):
+        """Returns all OrderItems with the given order ID"""
+        logger.info("Processing order query for %s ...", order_id)
+        return cls.query.filter(cls.order_id == order_id).all()
+
+    @classmethod
+    def create_item(cls, order_id, item_data):
+        """Creates a new item and adds it to an order"""
+        logger.info("Creating new item for order %s ...", order_id)
+        new_item = cls()
+        new_item.deserialize(item_data)
+        new_item.order_id = order_id
+        new_item.create()
+        return new_item
+
+    @classmethod
+    def find_item_in_order(cls, order_id, item_id):
+        """Finds a single item in an order"""
+        logger.info("Finding item %s in order %s ...", item_id, order_id)
+        return cls.query.filter_by(order_id=order_id, order_item_id=item_id).first()
+
+    @classmethod
+    def update_item_in_order(cls, order_id, item_id, data):
+        """Updates a single item in an order"""
+        logger.info("Updating item %s in order %s ...", item_id, order_id)
+        item = cls.find_item_in_order(order_id, item_id)
+        if not item:
+            return None
+        for key, value in data.items():
+            setattr(item, key, value)
+        item.update()
+        return item
+
+    @classmethod
+    def delete_item_from_order(cls, order_id, item_id):
+        """Deletes a single item from an order"""
+        logger.info("Deleting item %s from order %s ...", item_id, order_id)
+        item = cls.find_item_in_order(order_id, item_id)
+        if not item:
+            return None
+        item.delete()
+        return item
--- tests/factories.py
+++ tests/factories.py
@@ -3,18 +3,53 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from service.models import Orders, OrderItems
 
 
-class YourResourceModelFactory(factory.Factory):
-    """Creates fake pets that you don't have to feed"""
+class OrdersFactory(factory.Factory):
+    """
+    Factory class for creating Orders instances.
+    """
 
-    class Meta:  # pylint: disable=too-few-public-methods
-        """Maps factory to data model"""
+    class Meta:
+        """
+        Meta class for defining metadata options for the Orders factory.
+        """
 
-        model = YourResourceModel
+        model = Orders
 
-    id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
+    order_id = factory.Sequence(lambda n: n)
+    customer_id = factory.Sequence(lambda n: n)
+    order_date = factory.Faker("date_time")
+    status = factory.Iterator(
+        [
+            "pending",
+            "processing",
+            "shipped",
+            "delivered",
+            "cancelled",
+            "returned",
+            "refunded",
+        ]
+    )
+    tracking_number = factory.Faker("random_number", digits=10)
+    discount_amount = factory.Faker("random_number", digits=2)
 
-    # Todo: Add your other attributes here...
+
+class OrderItemsFactory(factory.Factory):
+    """
+    Factory class for creating instances of OrderItems model.
+    """
+
+    class Meta:
+        """
+        Meta class for defining metadata options for the OrderItems factory.
+        """
+
+        model = OrderItems
+
+    order_item_id = factory.Sequence(lambda n: n)
+    order_id = factory.SubFactory(OrdersFactory)
+    product_id = factory.Sequence(lambda n: n)
+    quantity = factory.Faker("random_number", digits=2)
+    price = factory.Faker("random_number", digits=2)
