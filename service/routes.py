d790999ff0ab134c5e8266c1030f48db182162d6
red <red@redrybin.me>
2024-03-04 22:37:59 +0000

    Refactor import statement in routes.py

--- service/routes.py
+++ service/routes.py
@@ -24,7 +24,7 @@ and Delete Pets from the inventory of pets in the PetShop
 from flask import jsonify, request, url_for, abort
 from flask import current_app as app  # Import Flask application
 from service.models import Orders
-from service.common import status  # HTTP Status Codes
+from service.common import status
 
 
 ######################################################################
@@ -32,7 +32,7 @@ from service.common import status  # HTTP Status Codes
 ######################################################################
 @app.route("/")
 def index():
-    """ Root URL response """
+    """Root URL response"""
     return (
         "Reminder: return some useful information in json format about the service here",
         status.HTTP_200_OK,
